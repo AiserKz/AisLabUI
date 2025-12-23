08aca7e6c13fadd87bd1d1658f342317d187f54a
AiserKz <aibekmiribekov0217@gmail.com>
2025-12-23 19:37:35 +0500

    modified:   ais-lab-ui/src/Layout/AppLayout.tsx 	modified:   ais-lab-ui/src/pages/Animations.tsx 	new file:   ais-lab-ui/src/transitions/PageTransitions.tsx

--- ais-lab-ui/src/Layout/AppLayout.tsx
+++ ais-lab-ui/src/Layout/AppLayout.tsx
@@ -1,12 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { Header } from "../components/Header";
 
+import PageTransitions from "@/transitions/PageTransitions";
+
 export function AppLayout() {
+  const location = useLocation();
+
   return (
     <div className="relative">
       <Header />
       <main className="mt-30 flex-1">
-        <Outlet />
+        <PageTransitions key={location.pathname}>
+          <Outlet />
+        </PageTransitions>
       </main>
     </div>
   );
